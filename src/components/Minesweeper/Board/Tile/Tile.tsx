b30651ba8a0d254ea681f3d5e389d65d5f128656
Justice Berteig <justice.berteig@berteig.com>
2024-04-05 09:36:39 -0400

    Made tiles have no contents when not revealed

--- src/components/Minesweeper/Board/Tile/Tile.tsx
+++ src/components/Minesweeper/Board/Tile/Tile.tsx
@@ -13,25 +13,37 @@ type TProps = {
 };
 
 function Tile(props: TProps) {
+    // Create list of classes to give the element
+    const classes: string[] = ["tile"];
+    if (
+        props.tile.isRevealed &&
+        props.tile.adjacentMines > 0 &&
+        props.tile.adjacentMines < 9
+    ) {
+        // If the tile is revealed and has between 1 and 8 adjacent mines
+        classes.push(numberToString(props.tile.adjacentMines));
+    }
+
+    // Create element for the contents of the tile
     let tileContents: ReactElement;
-    if (props.tile.hasMine) {
-        tileContents = <img src="mine.svg" className="tile-contents"></img>;
-    } else if (props.tile.adjacentMines > 0 && props.tile.adjacentMines < 9) {
-        tileContents = (
-            <span className="tile-contents">{props.tile.adjacentMines}</span>
-        );
+    if (props.tile.isRevealed && props.tile.hasMine) {
+        // If the tile is revealed and has a mine
+        tileContents = <img src="mine.svg"></img>;
+    } else if (
+        props.tile.isRevealed &&
+        props.tile.adjacentMines > 0 &&
+        props.tile.adjacentMines < 9
+    ) {
+        // If the tile is revealed and has between 1 and 8 adjacent mines
+        tileContents = <span>{props.tile.adjacentMines}</span>;
     } else {
-        tileContents = <span className="tile-contents"></span>;
+        // Otherwise
+        tileContents = <span></span>;
     }
 
     return (
         <button
-            className={
-                "tile" +
-                (numberToString(props.tile.adjacentMines)
-                    ? " " + numberToString(props.tile.adjacentMines)
-                    : "")
-            }
+            className={classes.join(" ")}
             onClick={() => {
                 // When the tile is left clicked
                 props.dispatch({
